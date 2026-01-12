5624d4bd3d3c9f390b0cccbe85daaae85740626a
cvionis <cvionis03@gmail.com>
2026-01-12 13:37:02 -0500

    initial commit

---
+++ build.bat
@@ -0,0 +1,73 @@
+@echo off
+setlocal
+
+:: --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ::
+:: Note: This script assumes that it will be called from project's root directory. ::
+:: --- --- --- --- --- --- --- --- --- --- --- -- ---- -- --- --- --- --- ---- --- ::
+
+:: --- Build from an environment where MSVC compiler tools exist ------------
+where /q cl || (
+  call C:\"Program Files"\"Microsoft Visual Studio"\2022\Community\VC\Auxiliary\Build\vcvarsall.bat x64 > nul
+)
+
+:: --- Prepare arguments ----------------------------------------------------
+set "release=0"
+set "asan=0"
+for %%a in (%*) do set "%%a=1"
+
+:: --- Prepare build directory ---------------------------------------------
+set root=%CD%
+if not exist %root%\build mkdir %root%\build
+
+:: --- Determine build configuration ---------------------------------------
+set debug=
+set cl_optimize_flags=
+
+if "%release%"=="1" (
+  set debug=0
+  set cl_optimize_flags=/O2
+  echo [release]
+) else (
+  set debug=1
+  set cl_optimize_flags=/Od
+  echo [debug]
+)
+
+:: --- Compiler flags -----------------------------------------------------
+set exe_name=kage.exe
+
+set cl_build_flags=/DBUILD_CLI=0 /DBUILD_DEBUG=%debug%
+set cl_warning_flags=/D_CRT_SECURE_NO_WARNINGS /wd4201 /wd4456 /wd4505 /W4 /Zc:strictStrings-
+set cl_common=/Fe:%exe_name% /nologo /FC /Zi /diagnostics:caret /std:c++20
+
+if "%asan%"=="1" (
+  set cl_common=%cl_common% /fsanitize=address
+  echo [asan enabled]
+)
+
+set compiler_flags=%cl_common% %cl_optimize_flags% %cl_build_flags% %cl_warning_flags% /MD
+
+:: --- Includes -----------------------------------------------------------
+set freetype="%root%\src\third_party\freetype-2.11.1"
+set raylib="%root%\src\third_party\raylib-5.0"
+
+set includes=
+set includes=%includes% /FI %freetype%\include\ft2build.h
+set includes=%includes% /I %freetype%\include
+set includes=%includes% /I ..\src\third_party\stb
+set includes=%includes% /I %raylib%\include
+
+:: -- Linker flags --------------------------------------------------------
+set linker_flags=/link %freetype%\build\freetype.lib winmm.lib shell32.lib %raylib%\build\raylib.lib /ignore:4099 /INCREMENTAL:no
+
+:: --- Build targets ------------------------------------------------------
+set targets=
+set targets=%targets% %root%\src\main.cpp
+
+:: --- Compile and link the main program  ---------------------------------
+pushd %root%\build
+cl.exe %compiler_flags% %includes% %targets% %linker_flags%
+
+:: --- Unset variables ----------------------------------------------------
+for %%a in (%*) do set "%%a=0"
+endlocal
---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include "base/base_inc.h"
+#include "os/os_inc.h"
+//#include "async/async_inc.h"
+
+#include "base/base_inc.cpp"
+#include "os/os_inc.cpp"
+//#include "async/async_inc.cpp"
+
+struct AppState {
+  Arena *arena;
+  OS_Handle window;
+  B32 quit;
+};
+
+void
+entry_point(void)
+{
+  os_init();
+  os_gfx_init();
+
+  S32 screen_w = 1280;
+  S32 screen_h = 720;
+
+  AppState app = {
+    .arena = arena_alloc_default(),
+    .window = os_window_open(S8("Kage"), screen_w, screen_h),
+  };
+
+  for (;;) {
+    OS_EventList *events = os_get_events();
+    for (OS_Event *e = events->first; e != 0; e = e->next) {
+      if (e->kind == OS_EventKind_WindowClose) {
+        app.quit = 1;
+      }
+    }
+    if (app.quit) {
+      break;
+    }
+  }
+
+  arena_release(app.arena);
+}
